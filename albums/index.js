c39c1aa5735eb72666d3250ede92d09e64945ba0
CICERO NEVES <cicero.info@gmail.com>
2017-11-23 20:43:36 -0500

    header style

--- albums/index.js
+++ albums/index.js
@@ -1,32 +1,12 @@
 // Import a library to help create a component
 import React from 'react';
-//one way
-//import ReactNative from 'react-native';
-//another better way
 import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
-// Create a component
-//one way
-/*
-const App = () => {
-  return (
-    <Text>Some Text<Text>
-  );
-};*/
 
-//another better way
-/* OLD HEADER
-const App = () => (
-    <Text>Some Text</Text>
-);
-*/
+// Create a component
 const App = () => (
     <Header />
 );
 
-
 // Render it to the device
-//one way
-//ReactNative.AppRegistry.registerComponent('albums', () => App);
-//another better way
 AppRegistry.registerComponent('albums', () => App);
