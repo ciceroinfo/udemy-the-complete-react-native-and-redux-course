739d204b89dc1b25ede7538b5f4e4dda7664e1bf
CICERO NEVES <cicero.info@gmail.com>
2017-11-25 12:31:40 -0500

    add button

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,12 +1,85 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+  const { title, artist, thumbnail_image, image, url } = album;
+  const {
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerContentStyle,
+    headerContentTextStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <View style={headerContentTextStyle}>
+            <Text>Title: </Text>
+            <Text style={headerTextStyle}>{title}</Text>
+          </View>
+          <View style={headerContentTextStyle}>
+            <Text>Artist: </Text>
+            <Text>{artist}</Text>
+          </View>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button onPressButton={() => Linking.openURL(url)}>Buy Now</Button>
+      </CardSection>
+
+    </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerContentTextStyle: {
+    alignItems: 'baseline',
+    flexDirection: 'row'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
